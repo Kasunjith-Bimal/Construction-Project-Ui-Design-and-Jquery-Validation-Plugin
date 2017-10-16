659e21ad2f3563e89f8c2f6da92aeb7bbcb0492b
Kasunjith-Bimal <kasunysoft@gmail.com>
2017-10-17 00:06:58 +0530

    Add Project Ui And Jquery Validation Plugin

---
+++ js/projectValidation.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+   
+    $('#project-form').validate({
+        rules: {
+          
+            formClientName: {
+                required: true,
+             
+            },
+            formLevel: {
+                required: true,
+             
+            },
+            formStatus: {
+                required: true,
+             
+            },
+            formStartDate: {
+                required: true,
+            
+            },
+            formEndDate: {
+                required: true,
+                greaterThan: "#formStartDate" 
+                
+            },
+            formdescription: "required",
+            formname: {
+                required: true,
+              
+            },
+        },
+        messages: {
+        
+            formClientName: {
+                required: "Please Select Client Name",
+              
+
+            },
+             formLevel: {
+                required: "Please Select Level Of the Project",
+              
+
+            },
+             formStatus: {
+                required: "Please Select Status Of the Project",
+              
+
+            },
+            formStartDate: {
+                required: "Please Select Start Date",
+               
+            },
+             formEndDate: {
+                required: "Please Select End Date",
+                greaterThan:"Please Select Valid Date"
+            },
+            formdescription: "please Enter Project Description",
+            formname: {
+                required: "Please Enter Project Name",
+               
+            },
+          
+        }
+
+    })
+});
